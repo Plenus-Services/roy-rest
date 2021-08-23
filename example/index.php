58d22e8f70803184007b780db51d01f08b3e6fd3
juanbautista0 <soyjuanbautista0@gmail.com>
2021-08-22 19:56:57 -0500

    readme format

--- example/index.php
+++ example/index.php
@@ -12,5 +12,6 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-(file_exists(__DIR__.'/../vendor/autoload.php')) ? require '../vendor/autoload.php' : die("bug");
-$app = new \RoyRest\Start(__DIR__.DIRECTORY_SEPARATOR."Modules".DIRECTORY_SEPARATOR, []);
+(file_exists(__DIR__ . '/../vendor/autoload.php')) ? require __DIR__ . '../vendor/autoload.php' : die("🐞");
+
+$app = new \RoyRest\Start(__DIR__ . DIRECTORY_SEPARATOR . "Modules" . DIRECTORY_SEPARATOR, []);
